ebd4104c27faf79b2bc60aba475d94b19ae8e8a5
DESKTOP-8LOP2SV\HP <h.azouagh@hocelot.com>
2025-11-05 16:30:31 +0100

    feat: initial project setup from Spring Initializr

---
+++ src/main/java/com/hicham/technicaltestinditex/TechnicalTestInditexApplication.java
@@ -0,0 +1,13 @@
+package com.hicham.technicaltestinditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestInditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestInditexApplication.class, args);
+	}
+
+}
